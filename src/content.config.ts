8888b22b7fb5feb52d90ed4efeae56584a7b7593
matocashew <info@matocashew.com>
2026-07-17 06:40:53 +0000

    Modified content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,12 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const resources = defineCollection({
-  type: "content",
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/resources"
+  }),
+
   schema: z.object({
     title: z.string(),
 
